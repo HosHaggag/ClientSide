0e8715488a761e338a38df0d5844377f43aa984c
Hossam Haggag <haggagdev@gmail.com>
2022-10-30 23:13:12 +0200

    supports sending and reciving

--- app/Program.cs
+++ app/Program.cs
@@ -8,85 +8,46 @@ using System.Text;
 // communication is established.
 public class SocketClient
 {
-    public static int Main(String[] args)
+   static void Main(string[] args)
     {
-        StartClient();
-        return 0;
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        int port = 9050;
+        TcpClient client = new TcpClient();
+        client.Connect(ip, port);
+        Console.WriteLine("client connected!!");
+        NetworkStream ns = client.GetStream();
+        Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+
+        thread.Start(client);
+
+        string s;
+        while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(s);
+            ns.Write(buffer, 0, buffer.Length);
+        }
+
+        client.Client.Shutdown(SocketShutdown.Send);
+        thread.Join();
+        ns.Close();
+        client.Close();
+        Console.WriteLine("disconnect from server!!");
+        Console.ReadKey();
     }
 
-    public static void StartClient()
+    static void ReceiveData(TcpClient client)
     {
-        byte[] bytes = new byte[1024];
+        NetworkStream ns = client.GetStream();
+        byte[] receivedBytes = new byte[1024];
+        int byte_count;
 
-        try
+        while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
         {
-            // Connect to a Remote server
-            // Get Host IP Address that is used to establish a connection
-            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
-            // If a host has multiple addresses, you will get a list of addresses
-            IPHostEntry host = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = host.AddressList[0];
-            Console.WriteLine("IP Address: " + ipAddress.ToString());
-            Console.WriteLine("IPHostEntry: " + host.AddressList.ToString());
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
-
-            // Create a TCP/IP  socket.
-            Socket sender = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect the socket to the remote endpoint. Catch any errors.
-            try
-            {
-                // Connect to Remote EndPoint
-                sender.Connect(remoteEP);
-  Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
-
-                // Encode the data string into a byte array.
-                string message = "";
-                while(true)
-                {
-                    Console.WriteLine("Enter a message to send to the server: ");
-
-                     message = Console.ReadLine();
-                     Console.WriteLine("Sending message: " + message);
-
-                  
-
-                    // Encode the data string into a byte array.
-                    byte[] msg = Encoding.ASCII.GetBytes(message);
-                    Console.WriteLine("Sending message: " + msg.ToString());
-
-                    // Send the data through the socket.
-                    int bytesSent = sender.Send(msg);
-                        Console.WriteLine("Sent {0} bytes to server.", bytesSent);
-
-            
-                 
-                }
-
-                   // Release the socket.
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-            }
-            catch (ArgumentNullException ane)
-            {
-                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-            }
-            catch (SocketException se)
-            {
-                Console.WriteLine("SocketException : {0}", se.ToString());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unexpected exception : {0}", e.ToString());
-            }
-
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());
+            Console.WriteLine("Server: "+Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
         }
     }
+
+
+
+    
 }
